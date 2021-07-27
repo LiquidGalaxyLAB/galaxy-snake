559f496e6bc1c2fa170e4367649f7fa3e789e622
leoruas <leo.coelho.ruas@gmail.com>
2021-07-27 10:48:08 -0300

    fixed sudo permission and log output in installer

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# Initialize sudo access
 
-sudo -v
-
-#Create directory if it doesnt exist yet
+#Create logs directory if it doesnt exist yet
 mkdir -p ./logs
 
 #Create file name with name as date
@@ -12,7 +9,7 @@ filename="$date.txt"
 
 # Add to log with timestamp
 time=$(date +%H:%M:%S)
-echo "[$time] Installing Galaxy Snake..." >> ./logs/$filename
+echo "[$time] Installing Galaxy Snake..." | tee -a ./logs/$filename
 
 # Open port 8114
 
@@ -23,22 +20,24 @@ RESULT=$LINE",8114"
 DATA=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | grep "8114"`
 
 if [ "$DATA" == "" ]; then
-    sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
+    sudo sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
 else
     time=$(date +%H:%M:%S)
-    echo "[$time] Port already open." >> ./logs/$filename
+    echo "[$time] Port already open." | tee -a ./logs/$filename
 fi
 
 # Install dependencies
 time=$(date +%H:%M:%S)
-echo "[$time] Installing dependencies..."
+echo "[$time] Installing dependencies..." | tee -a ./logs/$filename
 npm install 2>> ./logs/$filename
 
-# Server
+# Stop server if already started
+pm2 delete SNAKE_PORT:8114 2> /dev/null
 
+# Start server
 pm2 start index.js --name SNAKE_PORT:8114 2>> ./logs/$filename
 
 pm2 save 2>> ./logs/$filename
 
 time=$(date +%H:%M:%S)
-echo "[$time] Installation complete." >> ./logs/$filename
+echo "[$time] Installation complete." | tee -a ./logs/$filename
