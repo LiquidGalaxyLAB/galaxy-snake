349bff52687cb2db64d53ff4593f9ce9a57478a0
leoruas <leo.coelho.ruas@gmail.com>
2021-07-27 10:24:14 -0300

    updated installer

--- install.sh
+++ install.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
-
-echo "Installing Galaxy Snake" >$HOME/installer/projLog/snake.txt
-
 # Initialize sudo access
 
 sudo -v
 
+#Create directory if it doesnt exist yet
+mkdir -p ./logs
+
+#Create file name with name as date
+date=$(date +%m-%d-%y)
+filename="$date.txt"
+
+# Add to log with timestamp
+time=$(date +%H:%M:%S)
+echo "[$time] Installing Galaxy Snake..." >> ./logs/$filename
+
 # Open port 8114
 
-LINE=`cat /etc/iptables.conf | grep "tcp" | grep " 8100" | awk -F " -j" '{print $1}'`
+LINE=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | awk -F " -j" '{print $1}'`
 
 RESULT=$LINE",8114"
 
-DATA=`cat /etc/iptables.conf | grep "tcp" | grep " 8100" | grep "8114"`
+DATA=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | grep "8114"`
 
 if [ "$DATA" == "" ]; then
-    sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>>$HOME/installer/projLog/snake.txt
+    sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
 else
-    echo "Port already open"
+    time=$(date +%H:%M:%S)
+    echo "[$time] Port already open." >> ./logs/$filename
 fi
 
 # Install dependencies
-
-npm install
+time=$(date +%H:%M:%S)
+echo "[$time] Installing dependencies..."
+npm install 2>> ./logs/$filename
 
 # Server
 
-pm2 start index.js --name SNAKE_PORT:8114 2>>$HOME/installer/projLog/snake.txt
+pm2 start index.js --name SNAKE_PORT:8114 2>> ./logs/$filename
 
-pm2 save 2>>$HOME/installer/projLog/snake.txt
+pm2 save 2>> ./logs/$filename
 
-echo "Installation complete" >>$HOME/installer/projLog/snake.txt
+time=$(date +%H:%M:%S)
+echo "[$time] Installation complete." >> ./logs/$filename
